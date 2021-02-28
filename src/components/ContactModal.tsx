d149a16cdb668bec4b3449f64589fbf3da6474ec
Anh Dai <anhdai@MacBook-Air-Anh-Dai.local>
2021-02-28 16:53:10 +0700

    chore: styling contact popup

--- src/components/ContactModal.tsx
+++ src/components/ContactModal.tsx
@@ -17,8 +17,77 @@ export const ContactModal:React.FC<TContactModal> = (props: TContactModal) => {
   return (
     <Modal isOpen={isModalOpen} onClose={closeModal}>
       <ModalBody>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum et eligendi repudiandae
-        voluptatem tempore!
+      <div className="sm:flex">
+        <div className="sm:w-1/2">
+          <div className="mt-6">
+            <div className="inline-block border-b-2 border-blue-500 text-black font-medium">
+              Email
+            </div>
+            <div className="mt-2.5">info@webhouse.com</div>
+          </div>
+          <div className="mt-6">
+            <div className="inline-block border-b-2 border-blue-500 text-black font-medium">
+              Phone
+            </div>
+            <div className="mt-2.5">0909909999</div>
+          </div>
+          <div className="mt-6">
+            <div className="inline-block border-b-2 border-blue-500 text-black font-medium">
+              Address
+            </div>
+            <div className="mt-2.5">
+              420-744 West Hastings Street, Vancouver, BC, V6C 1A6, Canada
+            </div>
+          </div>
+        </div>
+        <div className="sm:w-1/2 sm:mt-0 mt-8">
+          <form>
+            <div>
+              <label htmlFor="contact-name" className="text-black font-medium">
+                Name
+              </label>
+              <input
+                className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-primary-input mt-2 px-3 py-2 rounded"
+                id="contact-name"
+                type="text"
+                placeholder="Your name"
+              />
+            </div>
+            <div className="mt-6">
+              <label htmlFor="contact-email" className="text-black font-medium">
+                Email
+              </label>
+              <input
+                className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-primary-input mt-2 px-3 py-2 rounded"
+                id="contact-email"
+                type="text"
+                placeholder="Your Email"
+              />
+            </div>
+            <div className="mt-6">
+              <label htmlFor="contact-email" className="text-black font-medium">
+                Phone Number
+              </label>
+              <input
+                className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-primary-input mt-2 px-3 py-2 rounded"
+                id="contact-phone"
+                type="text"
+                placeholder="Your Phone Number"
+              />
+            </div>
+            <div className="mt-6">
+              <label htmlFor="contact-email" className="text-black font-medium">
+                Message
+              </label>
+              <textarea
+                className="appearance-none block w-full bg-grey-lighter text-grey-darker border border-primary-input mt-2 px-3 py-2 rounded"
+                id="contact-message"
+                placeholder="Message"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
       </ModalBody>
       <ModalFooter>
         <Button className="w-full sm:w-auto" layout="outline" onClick={closeModal}>
